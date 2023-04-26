ae8faafb83f505b8f3a4b82b85ce81c7c3c06142
Mund Vetter <mundvetter@gmail.com>
2023-04-26 12:14:13 +0200

    auto collect

--- collect_results.py
+++ collect_results.py
@@ -1,36 +1,50 @@
 import os
+import re
 import pandas as pd
 import numpy as np
+import constants
 
-def read_results_files(parent_folder, name, tag):
+def read_results_files(parent_folder):
     results_dfs = []
+    unique_names_tags = set()
     
     for root, _, files in os.walk(parent_folder):
         for file in files:
-            if file == f"{name}_{tag}_results.csv":
+            match = re.match(r"(.+)-(.+)-results\.csv", file)
+            if match:
+                name, tag = match.groups()
+                unique_names_tags.add((name, tag))
                 file_path = os.path.join(root, file)
-                results_dfs.append(pd.read_csv(file_path))
-    
+                df = pd.read_csv(file_path)
+                df['name'] = name
+                df['tag'] = tag
+                results_dfs.append(df)
+
     if not results_dfs:
-        raise FileNotFoundError(f"No '{name}_{tag}_results.csv' files were found in '{parent_folder}'.")
-    
-    return pd.concat(results_dfs)
+        raise FileNotFoundError(f"No '*-*-results.csv' files were found in '{parent_folder}'.")
+
+    return pd.concat(results_dfs), unique_names_tags
 
 def calculate_mean_std(df, column):
     return df[column].mean(), df[column].std()
 
-def main(parent_folder, name, tag):
-    results_df = read_results_files(parent_folder, name, tag)
-    configurations = results_df['configuration'].unique()
+def main(parent_folder):
+    results_df, unique_names_tags = read_results_files(parent_folder)
 
-    print("Configuration, Mean, Standard Deviation")
-    for config in configurations:
-        config_df = results_df[results_df['configuration'] == config]
-        mean, std = calculate_mean_std(config_df, 'value')
-        print(f"{config}, {mean}, {std}")
+    for name, tag in unique_names_tags:
+        print(f"Name: {name}, Tag: {tag}")
+        print("Configuration, Mean, Standard Deviation")
+        
+        specific_results_df = results_df[(results_df['name'] == name) & (results_df['tag'] == tag)]
+        configurations = specific_results_df['configuration'].unique()
+
+        for config in configurations:
+            config_df = specific_results_df[specific_results_df['configuration'] == config]
+            mean, std = calculate_mean_std(config_df, 'value')
+            print(f"{config}, {mean}, {std}")
+
+        print("\n")
 
 if __name__ == "__main__":
-    parent_folder = "assets/checkpoints/2d_images"
-    name = "test_masked"
-    tag = "x"
-    main(parent_folder, name, tag)
+    parent_folder = constants.CHECKPOINTS_ROOT / "2d_images"
+    main(parent_folder)
