c156730f59ab0a2bbede354e414a0a2da15b0f2f
Mund Vetter <mundvetter@gmail.com>
2023-06-12 22:35:25 +0200

    hoofdletters

--- run_submitit.py
+++ run_submitit.py
@@ -69,5 +69,5 @@ if __name__ == "__main__":
         for i in range(args.n_runs):
             group_name = str(uuid.uuid4())[:8]
             for file_name in file_names:
-                executor.submit(main, PATH=str(Path(args.folder_name) / file_name), CONTROLLER_TYPE=controller_type, EPOCHS=args.n_epochs, NON_UNIFORM = args.non_uniform, MASK_RES=args.mask_res, LAMBDA_COST=args.lambda_cost, RUN_NAME=group_name, THRESHOLD=args.threshold, SIGMA=args.sigma, BATCH_SIZE=args.batch_size, LR=args.lr, ENCODING_TYPE=encoding_type, RENDER_RES=args.render_res, INV_PROB = not args.no_inv_prob, WEIGHT_DECAY = args.weight_decay, BN = args.bn, id = args.id)
+                executor.submit(main, PATH=str(Path(args.folder_name) / file_name), CONTROLLER_TYPE=controller_type, EPOCHS=args.n_epochs, NON_UNIFORM = args.non_uniform, MASK_RES=args.mask_res, LAMBDA_COST=args.lambda_cost, RUN_NAME=group_name, THRESHOLD=args.threshold, SIGMA=args.sigma, BATCH_SIZE=args.batch_size, LR=args.lr, ENCODING_TYPE=encoding_type, RENDER_RES=args.render_res, INV_PROB = not args.no_inv_prob, WEIGHT_DECAY = args.weight_decay, BN = args.bn, ID = args.id)
 
